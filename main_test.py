be3abf38d86e95fd2d48c3b9727d98c0cdc3e688
Kamil <kam.popczyk@gmail.com>
2019-06-06 16:09:54 +0200

    Test update
    Added new test.

--- main_test.py
+++ main_test.py
@@ -12,3 +12,15 @@ class TestMain(unittest.TestCase):
         ]
         correct_answer = (22, {"sad_pepe_compilation.gif", "yodeling_kid.avi"})
         self.assertEqual(correct_answer, main.calculate(usb_size, memes))
+
+    def test_calculate_bad_args(self):
+        memes = [
+            ("rollsafe.jpg", 205, 6),
+            ("sad_pepe_compilation.gif", 410, 10),
+            ("yodeling_kid.avi", 605, 12),
+        ]
+
+        with self.assertRaises(TypeError):
+            main.calculate('some random string', memes)
+            main.calculate('some random string', 3)
+            main.calculate(1, 1)
